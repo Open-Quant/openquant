227f6f6e9f549c17643a471a644c896e32e235e8
Soren Rood <sorenrood@gmail.com>
2020-08-18 15:44:28 -0700

    stubbed out functions to create the standard bars.

--- data_structures/standard_bars.py
+++ data_structures/standard_bars.py
@@ -1,2 +1,18 @@
 # The purpose of this file is to serve as the storage location for the functions that help to generate the standard bars.
 # Standard bars are a way of recreating the chonological time bars so as to better reflect the inflow and outflow of (dollars, volume etc)
+
+# Imports
+import pandas as pd
+
+
+# TODO write a really good desc/documentation 
+def dollar_bars():
+    print('dollar bars')
+
+# TODO write a really good desc/documentation 
+def tick_bars():
+    print('tick bars')
+
+# TODO write a really good desc/documentation 
+def volume_vars():
+    print('volume bars')
