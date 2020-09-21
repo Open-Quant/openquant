e84843e2cddea15f149671555082121256eea6ca
Soren Rood <sorenrood@gmail.com>
2020-09-21 13:50:20 -0700

    added init files to every file in src to make it a package

--- src/tests/standard_bars_test.py
+++ src/tests/standard_bars_test.py
@@ -1,4 +1,3 @@
 # This will test the functions in the financial_data_structures/standard_bars.py file
 
-from src.financial_data_structures import dollar_bars, tick_bars, volume_bars
-
+from src.financial_data_structures import standard_bars
