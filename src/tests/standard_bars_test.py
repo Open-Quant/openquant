884f311c4e4ca76dcc442a009db1a358eb7d35b3
Soren Rood <sorenrood@gmail.com>
2020-09-21 14:22:56 -0700

    working on turning dir into package

--- src/tests/standard_bars_test.py
+++ src/tests/standard_bars_test.py
@@ -1,3 +1,3 @@
 # This will test the functions in the financial_data_structures/standard_bars.py file
 
-from src.financial_data_structures import standard_bars
+from src import financial_data_structures
