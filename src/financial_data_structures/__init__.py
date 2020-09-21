7448351f8282e4cbc44aefd17c4ea4fc54771d9d
Soren Rood <sorenrood@gmail.com>
2020-09-21 12:44:00 -0700

    docs

---
+++ src/financial_data_structures/__init__.py
@@ -0,0 +1,2 @@
+from src.financial_data_structures.standard_bars import dollar_bars, \
+    tick_bars, volume_bars
---
+++ src/tests/standard_bars.py
@@ -0,0 +1,1 @@
+# This will test the functions in the financial_data_structures/standard_bars.py file
