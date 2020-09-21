3fe2828b5bdd98d38878701e3fa666443927d937
Soren Rood <sorenrood@gmail.com>
2020-09-21 13:03:24 -0700

    working on getting imports working for tests

---
+++ src/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Testing Standard Bar creation:
+
+* Dollar Bars
+* Volume Bars
+* Tick Bars
+"""
+
+from src.financial_data_structures.standard_bars import volume_bars, tick_bars, dollar_bars
--- src/tests/standard_bars_test.py
+++ src/tests/standard_bars_test.py
@@ -1,1 +1,4 @@
 # This will test the functions in the financial_data_structures/standard_bars.py file
+
+from src.financial_data_structures import dollar_bars, tick_bars, volume_bars
+
