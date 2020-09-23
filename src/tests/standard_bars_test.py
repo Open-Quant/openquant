2d6fd147d843fd44e7455ad341268a787192af5f
Soren Rood <sorenrood@gmail.com>
2020-09-23 13:18:06 -0700

    wrote two tests to check for functions not erroring

--- src/tests/standard_bars_test.py
+++ src/tests/standard_bars_test.py
@@ -1,21 +1,24 @@
 """
 This will test the functions in the financial_data_structures/standard_bars.py file
 """
+
 import sys
 import os
 import inspect
 import pandas as pd
 
+# Hacky stuff that I want to replace with real idiomatic pyhon code as soon as possible. (packages)
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
-
 from financial_data_structures import standard_bars
 
-"""
-This validates that the dollar_bars() function does not error
-"""
-def standard_bars_test(dataframe):
+
+
+def dollar_bars_test(dataframe):
+    """
+    This validates that the dollar_bars() function does not error
+    """
     list0 = standard_bars.dollar_bars(dataframe, 'close', .05)
     if len(list0) > 0:
         pass
@@ -23,10 +26,22 @@ def standard_bars_test(dataframe):
         print('error')
 
 
-df = pd.read_csv('../data/raw/raw_data.csv')
-print(df)
+def dollar_bars_df_test(dataframe):
+    """
+    This validates that the dollar_bars_df() function returns a correct ammount of columns
+    """
+    df = standard_bars.dollar_bar_df(dataframe, 'close', .05)
+    if len(df.columns) > 0:
+        pass
+    else:
+        print('error')
 
-standard_bars_test(df)
+
+df = pd.read_csv('../data/raw/raw_data.csv')
+
+dollar_bars_test(df)
+dollar_bars_df_test(df)
+
 
 # standard_bars.dollar_bar_df()
 
