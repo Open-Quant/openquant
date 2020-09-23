ad0c78e82b8d33867a3fc6657511275cdb23e7f0
Soren Rood <sorenrood@gmail.com>
2020-09-22 22:15:12 -0700

    removed things that did not work

--- src/tests/standard_bars_test.py
+++ src/tests/standard_bars_test.py
@@ -2,8 +2,3 @@
 import sys
 import os
 
-from src import financial_data_structures
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__standard_bars), '..')))
-
-import standard_bars
