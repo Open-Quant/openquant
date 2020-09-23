425efd0c410ba91584b53ddc7d46e0c1dc472bd7
Soren Rood <sorenrood@gmail.com>
2020-09-23 12:23:51 -0700

    typo fix

--- src/__init__.py
+++ src/__init__.py
@@ -10,7 +10,7 @@ import src.filters as filters
 import src.financial_data_structures as financial_data_structures
 import src.labeling as labeling
 import src.models as models
-import src.portfolio_metrics as portfolio metrics
+import src.portfolio_metrics as portfolio_metrics
 import src.reports as reports
 import src.tests as tests
 import src.utils as utils
