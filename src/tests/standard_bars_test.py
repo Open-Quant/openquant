35a0865da3175770697a0af8ef0eac5da6ca7adb
Soren Rood <sorenrood@gmail.com>
2020-09-22 22:14:29 -0700

    committed uncommited edits.

--- src/tests/standard_bars_test.py
+++ src/tests/standard_bars_test.py
@@ -1,3 +1,9 @@
 # This will test the functions in the financial_data_structures/standard_bars.py file
+import sys
+import os
 
 from src import financial_data_structures
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__standard_bars), '..')))
+
+import standard_bars
